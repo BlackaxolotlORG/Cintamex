f0fd7ad1013bfe5df95ba2c8b1cba66f83000695
Diego Lopez-Araiza Soto <140852928+BlackaxolotlORG@users.noreply.github.com>
2024-05-29 17:34:55 -0600

    e

--- src/pages/Catalogue/Catalogue.jsx
+++ src/pages/Catalogue/Catalogue.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './Catalogue.module.css';
 import Download from '../../components/Download/Download';
 function Catalogue() {
-
   return (
     <Download />
   );

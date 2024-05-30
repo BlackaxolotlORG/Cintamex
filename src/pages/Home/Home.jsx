9d1c2e9d3b604a0c87a246142bda1302ca3a9550
Joseph Shakalo paz <Josephshakalopaz@joseph-shakalo.local>
2024-05-29 21:26:24 -0600

    avance en pagian de inicio

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,23 @@
+import React from 'react';
+import styles from './Home.module.css';
 import Download from '../../components/Download/Download';
 
-
 function Home() {
-    return (
-        <>
-             <h1>Homepage</h1>
-             <Download />
-        </>
+  return (
+    <div className={styles.homepage}>
+      <div className={styles.header}>
+        <h1>Cintamex, pega al ritmo de México </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper eros eget tincidunt ultrices.</p>
+        <button className={styles.shopNowBtn}>Ver Productos</button>
+      </div>
+      <div className={styles.newCollection}>
+        <h2>Nuevos Productos</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper eros eget tincidunt ultrices.</p>
+        
+      </div>
+      <Download />
+    </div>
+  );
+}
 
-    )
-  }
-  
-  export default Home
-  
+export default Home;

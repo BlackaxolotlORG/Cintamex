c93cd9823fe6ab4c76e1a2a2dc1fd089b186ee21
IsaacShakalo <ishakalopaz@gmail.com>
2024-06-04 11:57:13 -0600

    cambios contacto

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,10 +3,40 @@ import styles from './contact.module.css'
 function Contact() {
 
     return (
+        <div className={styles.contactanos}>
+           
+        <div className={styles.contactus}>
+        <h1>Contactanos</h1>
+        </div>
+
+        <div className={styles.columns}>
+
+       
+
+        <div className={styles.columns1}>
+        <img src="../../src/assets/Ubi.png" alt="Ubicacion" />
+        <h2>Ubicacion</h2>
+        {/* <h3>alle Los Remedios num.11 Col. San Luis Tlatilco Naucalpan de Juarez 53630</h3>*/}
+        </div>
+
+        <div className={styles.columns1}>
+        <img src="../../src/assets/Telefono.png" alt="Telefono" />
+        <h2>Telefono</h2>
+        {/*<h3> + 52 5585567960</h3> */} 
+        </div>
+
+        <div className={styles.columns1}>
+        <img src="../../src/assets/Mail.png" alt="Mail" />
+        <h2>Mail</h2>
+        {/*<h3>EDUARDOREZNIK@CINTAMEX.COM.MX</h3> */} 
+        </div>
+
+        </div>
+
         <div className={styles.background}>
             <div>
-            <h1>Contactanos</h1>
-             <p> Telefono: +52 55 8556 7950 </p>
+            <h1>Mandanos Un Mensaje</h1>
+            
             </div>
         
              <div>
@@ -20,10 +50,8 @@ function Contact() {
 
                     
                     <button>Enviar</button>
-                </form>
 
-
-                <div className={styles.socialMedia}>
+                    <div className={styles.socialMedia}>
                 <a href="https://www.linkedin.com/in/eduardo-reznik-0941a2170/" target="_blank" rel="noopener noreferrer">
                 <img src="../../src/assets/Linkedin.png" alt="Linkedin" />
                 </a>
@@ -37,7 +65,12 @@ function Contact() {
                 </a>
 
                 </div>
+                </form>
+
+
+               
              </div>
+        </div>
         </div>
 
     )

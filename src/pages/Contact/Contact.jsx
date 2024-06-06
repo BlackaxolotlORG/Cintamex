f7ae72ea393e61c08834992e10f4ac5cc11c19da
Diego Lopez-Araiza Soto <140852928+BlackaxolotlORG@users.noreply.github.com>
2024-06-06 16:58:29 -0600

    contact fix

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -35,11 +35,25 @@ function Contact() {
 
         <div className={styles.background}>
             <div>
+            
+            </div>
+            <div>
             <h1>Mandanos Un Mensaje</h1>
             
             </div>
-        
-             <div>
+            <div className={styles.formcontent}>
+            <div className={styles.map}>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3761.801009485496!2d-99.24061732107111!3d19.464144434951194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d203ad67af6b75%3A0xcef9d68c5c5cfd25!2sC.%20Los%20Remedios%2011%2C%20San%20Luis%20Tlatilco%2C%2053630%20Naucalpan%20de%20Ju%C3%A1rez%2C%20M%C3%A9x.!5e0!3m2!1sen!2smx!4v1717714047977!5m2!1sen!2smx"
+        width="100%"
+        height="100%"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+             <div className={styles.contactform}>
                 <form>
                     <label for="name">Nombre:</label>
                     <input type="text" id="name" name="name"></input>
@@ -70,6 +84,8 @@ function Contact() {
 
                
              </div>
+            </div>
+            
         </div>
         </div>
 

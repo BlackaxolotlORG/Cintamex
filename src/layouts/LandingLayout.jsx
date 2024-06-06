35cc1d5dab3c48805ec7d1a085173503ba945071
Diego Lopez-Araiza Soto <140852928+BlackaxolotlORG@users.noreply.github.com>
2024-06-06 14:11:48 -0600

    algunos cambios de estilo y contenido

--- src/layouts/LandingLayout.jsx
+++ src/layouts/LandingLayout.jsx
@@ -1,24 +1,28 @@
 import { Link } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import styles from './LandingLayout.module.css';
-
+import Logo from '../assets/Logo.jpeg'
 function Landinglayout() {
     return (
       <div>
         <div className={styles.navbar}>
+        <Link className={styles.navlink} to="/"> <img className={styles.logonav} src={Logo}></img></Link>
               <ul >
-              <Link to="/"><li><a>Inicio</a></li></Link>
+             
+              <Link className={styles.navlink} to="/"><li><a>Inicio</a></li></Link>
 
-              <Link to="/about"><li><a>Nosotros</a></li></Link>
-              <Link to="/products"><li><a>Productos</a></li></Link>
-              <Link to="/contact"><li><a>Contacto</a></li></Link>
+              <Link className={styles.navlink} to="/about"><li><a>Nosotros</a></li></Link>
+              <Link className={styles.navlink} to="/products"><li><a>Productos</a></li></Link>
+              <Link className={styles.navlink} to="/contact"><li><a>Contacto</a></li></Link>
 
   </ul>
               </div>
         <Outlet /> {/* This will render child routes here */}
-        {/* <div className={styles.footer}>
+       <div className={styles.footer}>
+       <Link className={styles.navlink} to="/" onClick={() => window.scrollTo(0, 0)}> <img className={styles.logonav} src={Logo}></img></Link>
           Hello this is a footer
-        </div> */}
+        </div> 
+
       </div>
     );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,18 +9,39 @@ function Home() {
     <div className={styles.homepage}>
       <div className={styles.header}>
         <h1>Cintamex, pega al ritmo de México </h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper eros eget tincidunt ultrices.</p>
-       <Link to ='/products'><button className={styles.shopNowBtn}>Ver Productos</button></Link> 
+        <p>Cintamex es una empresa 100 % mexicana que se especializa en la distribución de productos de empaque. Su principal fortaleza es el servicio al cliente, ofreciendo productos de calidad certificada al mejor precio del mercado y manteniendo siempre seriedad en sus tratos y operaciones.</p>
+       <Link to ='/products'><button className={styles.shopNowBtn} onClick={() => window.scrollTo(0, 0)}>Ver Productos</button></Link> 
       </div>
       <div className={styles.newCollection}>
         <h2>Nuevos Productos</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper eros eget tincidunt ultrices.</p>
+        <div className={styles.newproducts}>
+          <div className={styles.newproduct}>
+            <h3>Titulo</h3>
+            <img></img>
+            Hello
+            <button className={styles.view}>View</button>
+          </div>
+          <div className={styles.newproduct}>
+          <h3>Titulo</h3>
+            Hello
+            <button className={styles.view}>View</button>
+          </div>
+          <div className={styles.newproduct} >
+          <h3>Titulo</h3>
+            Hello
+            <button className={styles.view}>View</button>
+          </div>
+        </div>
         
       </div>
       <Download />
+      <div>
+        <h2>Nuestros Valores</h2>
+      </div>
       <div className={styles.newCollection}>
-        <h2>Quienes Somos</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper eros eget tincidunt ultrices.</p>
+        <h2>Nuestra Vision</h2>
+        <p>Ser reconocidos como el líder indiscutible en la distribución de productos de empaque en México, expandiendo nuestra presencia a mercados internacionales y estableciendo estándares de excelencia en servicio al cliente. Aspiramos a ser pioneros en la adopción de prácticas sostenibles en nuestra industria, contribuyendo activamente a la economía circular y al bienestar de nuestras comunidades.</p>
         
       </div>
     </div>

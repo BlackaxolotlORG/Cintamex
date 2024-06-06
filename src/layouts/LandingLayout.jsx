e9a6043f1386e0490c5fbc664d685d5ec4c204ad
Diego Lopez-Araiza Soto <140852928+BlackaxolotlORG@users.noreply.github.com>
2024-06-06 15:04:47 -0600

    arreglando footer

--- src/layouts/LandingLayout.jsx
+++ src/layouts/LandingLayout.jsx
@@ -20,7 +20,26 @@ function Landinglayout() {
         <Outlet /> {/* This will render child routes here */}
        <div className={styles.footer}>
        <Link className={styles.navlink} to="/" onClick={() => window.scrollTo(0, 0)}> <img className={styles.logonav} src={Logo}></img></Link>
-          Hello this is a footer
+          <div>
+            <h3>Informacion de Contacto</h3>
+            <div>
+              <p>Menu</p>
+              <ul >
+             
+              <Link className={styles.navlinkfoot} to="/"><li><a>Inicio</a></li></Link>
+
+              <Link className={styles.navlinkfoot} to="/about"><li><a>Nosotros</a></li></Link>
+              <Link className={styles.navlinkfoot} to="/products"><li><a>Productos</a></li></Link>
+              <Link className={styles.navlinkfoot} to="/contact"><li><a>Contacto</a></li></Link>
+
+  </ul>
+            </div>
+            <div>
+              <p>Ubicacion:</p>
+              <p>Telefono: </p>
+              <p>Email: </p>
+            </div>
+          </div>
         </div> 
 
       </div>
